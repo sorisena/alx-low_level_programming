3df1404f683804d4c7612e1e44b35cd62aa45b14
sorisena <seenaabaayee@gmail.com>
2023-06-15 00:31:41 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - program that print puts function
+ * Return:0(met your target
+ */
+int main(void)
+{
+puts("\ "Programming is like building a multilingual puzzle");
+return(0);
+}
