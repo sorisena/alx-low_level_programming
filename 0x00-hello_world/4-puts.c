543b88a6c7b15382bc1f39cfb728f199c39bd2b6
sorisena <seenaabaayee@gmail.com>
2023-06-15 00:39:47 -0700

    vi 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
  * main - program that print puts function
- * Return:0(met your target
+ * Return:0(met your target)
  */
 int main(void)
 {
-puts("\ "Programming is like building a multilingual puzzle");
-return(0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
