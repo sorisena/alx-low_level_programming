33fb12e4f333e6d094e8cce8316dd3dceb09f57d
sorisena <seenaabaayee@gmail.com>
2023-06-16 04:30:52 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - alphabet
+ * Return:0 (succes)
+ *
+ */
+int main(void)
+{
+char low, e, q;
 
+e = 'e';
+q = 'q';
+
+for (low = 'a'; low <= 'z'; low++)
+{
+if (low != e  && low != q)
+        putchar(low);
+}
+putchar('\n');
+return (0);
+}
