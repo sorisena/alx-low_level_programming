a5a2a3433cc688561353a1267672f9b61fd448bf
sorisena <seenaabaayee@gmail.com>
2023-06-16 05:15:20 -0700

    ./7-print_tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/** * main - entry point
+ *  * Return:0 (succes)
+ *
+ */
+int main(void)
+{
+char low;
+for (low = 'a'; low <= 'z'; low--)
+putchar(low);
+putchar('\n'); 
+return (0);
+}
