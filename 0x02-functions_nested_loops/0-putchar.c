d9545920db180ff8ea079f88eda739607319ddc7
sorisena <seenaabaayee@gmail.com>
2023-06-20 00:49:52 -0700

    0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,7 +5,8 @@
 */
 int main(void)
 {
-char c[] = "_putchar";
+
+char c[] = "_Putchar";
 int i;
 for (i = 0; i < 8; i++)
 {
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,3 @@
-#ifndef main_h
-#define main_h
 int _putchar(int c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -13,5 +11,4 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
-#endif
 
