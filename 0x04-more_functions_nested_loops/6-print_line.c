6b4b211503f34f1034df318bcdd94976418bc9bf
sorisena <seenaabaayee@gmail.com>
2023-06-22 01:59:01 -0700

    print nyumber og lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *print_line - print 0 to 14 10 times
+ @n: input paramater
+ * Return: nothing
+ *
+ */
+void print_line(int n)
+{
+	int x;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (x = 0; x < n; x++)
+		{
+			_putchar(95);
+		}
+		_putchar('\n');
+	}
+}
