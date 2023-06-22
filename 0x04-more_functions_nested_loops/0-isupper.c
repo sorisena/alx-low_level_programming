801811b55a433fff7bb5b259cd0dd77d312fefbf
sorisena <seenaabaayee@gmail.com>
2023-06-21 23:57:07 -0700

    print upper and lower case

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *_isupper - print upper and lower case.
+ *@x:input parameter
+ * Return: 1 for upper case and 0 for lower case.
+ */
+
+int _isupper(int x)
+
+{
+	if (x >= 65 && x <= 90)
+	{ return (1);
+	}
+	return (0);
+}
